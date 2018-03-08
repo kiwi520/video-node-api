ccfa44ac95ebf897f8809f53e05f1ac09f81c9be
kiwi <kiwi@kiwis-iMac.lan>
2018-03-08 12:09:17 +0800

    update config

--- config/sequelize/db-default.js
+++ config/sequelize/db-default.js
@@ -1,9 +1,9 @@
 var config = {
     dialect: 'mysql',
     database: 'video',
-    username: 'homestead',
-    password: 'secret',
-    host: '192.168.33.10',
+    username: 'root',
+    password: 'zhmgws2017',
+    host: '127.0.0.1',
     port: 3306
 };
 
