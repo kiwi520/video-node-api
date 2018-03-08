943bfe055dd33960c15b5b600fb2e5019085c955
kiwi <kiwi@kiwis-iMac.lan>
2018-03-08 10:49:21 +0800

    update log level

--- middleware/mi-log/logger.js
+++ middleware/mi-log/logger.js
@@ -6,7 +6,7 @@ const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
 const baseInfo = {
     appLogLevel: 'error',  // 指定记录的日志级别
     dir: 'logs',        // 指定日志存放的目录名
-    env: 'dev',   // 指定当前环境，当为开发环境时，在控制台也输出，方便调试
+    env: 'pro',   // 指定当前环境，当为开发环境时，在控制台也输出，方便调试
     projectName: 'koa2-tutorial',  // 项目名，记录在日志中的项目信息
     serverIp: '0.0.0.0'        // 默认情况下服务器 ip 地址
 }
